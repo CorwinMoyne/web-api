bb3f81410f3b0030fe9e499427765e24ee0ab110
Corwin Moyne <c_moyne@yahoo.com>
2019-06-26 09:09:21 +0100

    LocationVenueName mapped to Venue

--- starting/TheCodeCamp/Data/CampMappingProfile.cs
+++ starting/TheCodeCamp/Data/CampMappingProfile.cs
@@ -12,7 +12,7 @@ namespace TheCodeCamp.Data
     {
         public CampMappingProfile()
         {
-            CreateMap<Camp, CampModel>();
+            CreateMap<Camp, CampModel>().ForMember(c => c.Venue, opt => opt.MapFrom(m => m.Location.VenueName));
         }
     }
 }
